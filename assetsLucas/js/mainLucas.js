7e884649778165e4ff9fa7317a12f45d45bc89da
Lucas <lucascow@users.noreply.github.com>
2017-01-25 16:09:14 +0800

    Update mainLucas.js

--- assetsLucas/js/mainLucas.js
+++ assetsLucas/js/mainLucas.js
@@ -1,1 +1,13 @@
+$( document ).ready(function() {
 
+ $(".masthead__menu-item").each(
+  function(){
+    $( this ).on( "click", function() {
+      $(".masthead__menu-item").removeClass("links-activeItem");
+      $( this ).addClass("links-activeItem");
+      
+      });
+    }
+  );
+  
+});
