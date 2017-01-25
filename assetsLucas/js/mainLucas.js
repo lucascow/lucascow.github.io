ee13dacae0e93e1d4ebff47dd14012383c822023
Lucas <lucascow@users.noreply.github.com>
2017-01-25 16:21:36 +0800

    Update mainLucas.js

--- assetsLucas/js/mainLucas.js
+++ assetsLucas/js/mainLucas.js
@@ -3,11 +3,18 @@ $( document ).ready(function() {
  $(".masthead__menu-item").each(
   function(){
     $( this ).on( "click", function() {
-      $(".masthead__menu-item").removeClass("links-activeItem");
-      $( this ).addClass("links-activeItem");
-      console.log("masthead__menu-item:I am caleld");
+      //$(".masthead__menu-item").removeClass("links-activeItem");
+      //$( this ).addClass("links-activeItem");
+      //console.log("masthead__menu-item:I am caleld");
+      window.activeNaviItem =   $( this );
       });
     }
   );
+
+  if(window.activeNaviItem)
+  {
+    $(window.activeNaviItem).addClass("links-activeItem");
+  }
+  
   
 });
